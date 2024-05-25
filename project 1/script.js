4040338c57758fe8e2ac34c6187d35594082c070
Ankush Das <workwithankush1111@gmail.com>
2024-05-25 18:29:50 +0530

    Delete project 1 directory

--- project 1/script.js
+++
@@ -1,7 +0,0 @@
-alert("Created By Ankush Das")
-
-function showThankYouMessage(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-    document.querySelector('.form-section').style.display = 'none';
-    document.querySelector('.thank-you-section').style.display = 'block';
-}
